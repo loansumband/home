17ee64bd0f2f790ad7d325057f103c688ffdf6bf
Kairos Wong <kairos0319@gmail.com>
2026-05-02 22:18:44 -0500

    fishies

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-let str = "OANSUM"
-let letter = document.createElement("img")
-let dir = "gifs\\ALPHABET\\3DFONT\\"
-dir = "images\\"
-letter.src = dir + "54\\CAP2\\L.GIF"
-letter.width = 200
-document.getElementById("title").appendChild(letter)
-for(let l of str){
-	let num = Math.floor(Math.random() * 73)
-	num = 55
-    letter = document.createElement("img")
-	letter.src = dir +num+"\\LOW2\\"+l+".GIF"
-    letter.width = 100
-	document.getElementById("title").appendChild(letter)
-}
+// let str = "OANSUM"
+// let letter = document.createElement("img")
+// let dir = "gifs\\ALPHABET\\3DFONT\\"
+// dir = "images\\"
+// letter.src = dir + "54\\CAP2\\L.GIF"
+// letter.width = 200
+// document.getElementById("title").appendChild(letter)
+// for(let l of str){
+// 	let num = Math.floor(Math.random() * 73)
+// 	num = 55
+//     letter = document.createElement("img")
+// 	letter.src = dir +num+"\\LOW2\\"+l+".GIF"
+//     letter.width = 100
+// 	document.getElementById("title").appendChild(letter)
+// }
 
 
 Array.from(document.getElementsByClassName("movable")).forEach(img => {
@@ -23,47 +23,56 @@ Array.from(document.getElementsByClassName("movable")).forEach(img => {
 	let mousestartX = 0;
 	let mousestartY = 0;
 	let currnode;
-  
-	document.addEventListener("mousedown", (e) => {
-	  isDragging = true;
-	//   const rect = img.getBoundingClientRect();
-	  mousestartX = e.clientX;
-	  mousestartY = e.clientY;
-	  currnode = e.target
-	  startingX = e.target.style.left.substr(0, currnode.style.top.length - 2)
-	  startingY = e.target.style.top.substr(0, currnode.style.top.length - 2)
-	  e.target.style.cursor = "grabbing";
 
-	});
-	document.addEventListener("mousemove", (e)=>{
-		if(isDragging == false || !currnode.isEqualNode(e.target)){
-			return
-		}
-		e = e || window.event;
+	function getClientX(e) {
+		return e.touches ? e.touches[0].clientX : e.clientX;
+	}
+	
+	function getClientY(e) {
+		return e.touches ? e.touches[0].clientY : e.clientY;
+	}
+	
+	document.addEventListener("mousedown", startDrag);
+	document.addEventListener("touchstart", startDrag);
+	
+	document.addEventListener("mousemove", drag);
+	document.addEventListener("touchmove", drag);
+	
+	document.addEventListener("mouseup", endDrag);
+	document.addEventListener("touchend", endDrag);
+	
+	function startDrag(e) {
+		isDragging = true;
+		
+		mousestartX = getClientX(e);
+		mousestartY = getClientY(e);
+		
+		currnode = e.target;
+		
+		startingX = parseInt(currnode.style.left) || 0;
+		startingY = parseInt(currnode.style.top) || 0;
+		
+		currnode.style.cursor = "grabbing";
+	}
+	
+	function drag(e) {
+		if (!isDragging || !currnode) return;
+
 		e.preventDefault();
-		elmnt = currnode
-		// calculate the new cursor position:
-		pos3 = e.clientX;
-		pos4 = e.clientY;
-		
-		pos1 = pos3 - e.clientX;
-		pos2 = pos4 - e.clientY;
-		
-		left = parseInt(startingX) + e.clientX - mousestartX
-		offtop = parseInt(startingY) + e.clientY - mousestartY
 
+		let clientX = getClientX(e);
+		let clientY = getClientY(e);
 
-		// set the element's new position:
-		elmnt.style.top = offtop + "px";
-		elmnt.style.left = left + "px";
+		let left = startingX + (clientX - mousestartX);
+		let top = startingY + (clientY - mousestartY);
 
-	})
-	document.addEventListener("mouseup", (e)=>{
-		// stop moving when mouse button is released:
-		document.onmouseup = null;
-		document.onmousemove = null;
-		e.target.style.cursor = "grab"
-		isDragging = false
-	})
+		currnode.style.left = left + "px";
+		currnode.style.top = top + "px";
+	}
+	
+	function endDrag(e) {
+		isDragging = false;
+		if (currnode) currnode.style.cursor = "grab";
+	}
   
   });
